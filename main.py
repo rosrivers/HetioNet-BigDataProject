6db3f1f1688067d464c8fe80541e7de564870de6
Rosa Rivera <riverasoto.rosa@gmail.com>
2024-04-27 19:05:55 -0400

    Refactor data processing and saving***

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import pandas as pd
 
-#read the data
-nodes_df = pd.read_csv('nodes_test.tsv', sep='\t')
-edges_df = pd.read_csv('edges_test.tsv', sep='\t')
+nodes_df = pd.read_csv('nodes_test.tsv', sep='\t', on_bad_lines='skip') #this will skip the bad lines
+edges_df = pd.read_csv('edges_test.tsv', sep='\t', on_bad_lines='skip')
 
-# print the data
 print("Nodes DataFrame:")
 print(nodes_df.head())
 print("Edges DataFrame:")
 print(edges_df.head())
 
-nodes_df.to_csv('nodes_output.tsv', sep='\t', index=False) # Save the nodes data to a new TSV file
-edges_df.to_csv('edges_output.tsv', sep='\t', index=False) # Save the edges data to a new TSV file
+nodes_df.to_csv('nodes_output.tsv', sep='\t', index=False) #this will save the data to a new file called nodes_output.tsv and do the same for edges
+edges_df.to_csv('edges_output.tsv', sep='\t', index=False)
 
-print("Data has been processed and saved.")
-
-
+print("Data has been processed and saved.") #this will print out the message "Data has been processed and saved." to the console which is just for me
